24b71f2d755e976ede6b53af2ef6abd61420a1f8
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-16 12:56:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -71,9 +71,9 @@ import streamlit as st
 import pandas as pd
 
 data = pd.DataFrame({
-    'index': ['Cincinnati', 'San Francisco', 'Pittsburgh'],
+    'Creal Crop': ['Cincinnati', 'San Francisco', 'Pittsburgh'],
     'Area Coverage (1000ha) in 2021/22 MY vs Cereal Crop': [6, 8, 9],
-}).set_index('index')
+}).set_Creal Crop('Cereal Crop')
 
 st.bar_chart(data)
 
