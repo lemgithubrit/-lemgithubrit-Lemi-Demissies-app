fb50acb436559663bee8cd3d038604a92ae8d31a
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-16 10:46:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ option = st.selectbox(
 #ADD TABLE
 import pandas as pd 
 # Creating the dataframe df
-df = pd.DataFrame({'Roll Number': ['20CSE29', '20CSE49', '20CSE36', '20CSE44', '20CSE36', '20CSE44'],
+df = pd.DataFrame({'Teff Yield in 2020/21': ['20CSE29', '20CSE49', '20CSE36', '20CSE44', '20CSE36', '20CSE44'],
                    'Name': ['Amelia', 'Sam', 'Dean', 'Jessica', 'Dean', 'Jessica'],
                    'Marks In Percentage': ['Amelia', 'Sam', 'Dean', 'Jessica', 'Dean', 'Jessica'],
                    'Grade': ['A', 'A', 'C', 'B', 'C', 'B'],
