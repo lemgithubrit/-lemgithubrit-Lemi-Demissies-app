60ad4b410aabf41c64e4199351940c9cda684d33
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-16 10:01:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,15 @@ st.write("<h1 style='text-align: center; color:white;'> Streamlit is the Fastest
 
 
 
-
+import pandas as pd
+  
+  
+# Creating the dataframe df
+df = pd.DataFrame({'Roll Number': ['20CSE29', '20CSE49', '20CSE36', '20CSE44'],
+                   'Name': ['Amelia', 'Sam', 'Dean', 'Jessica'],
+                   'Marks In Percentage': [97, 90, 70, 82],
+                   'Grade': ['A', 'A', 'C', 'B'],
+                   'Subject': ['Physics', 'Physics', 'Physics', 'Physics']})
+  
+# Printing the dataframe
+df
