b62f9cbd07787283a6dca71aeec38d225898f5ec
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-16 22:00:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -178,26 +178,6 @@ st.write("<h1 style='text-align: center; color:white;'> Streamlit is the Fastest
 
 
 
-sunset_imgs = [
-    'https://unsplash.com/photos/-IMlv9Jlb24/download?force=true',
-    'https://unsplash.com/photos/ESEnXckWlLY/download?force=true',
-    'https://unsplash.com/photos/mOcdke2ZQoE/download?force=true',
-    'https://unsplash.com/photos/GPPAjJicemU/download?force=true',
-    'https://unsplash.com/photos/JFeOy62yjXk/download?force=true',
-    'https://unsplash.com/photos/kEgJVDkQkbU/download?force=true',
-    'https://unsplash.com/photos/i9Q9bc-WgfE/download?force=true',
-    'https://unsplash.com/photos/tIL1v1jSoaY/download?force=true',
-    'https://unsplash.com/photos/-G3rw6Y02D0/download?force=true',
-    'https://unsplash.com/photos/xP_AGmeEa6s/download?force=true',
-    'https://unsplash.com/photos/4iTVoGYY7bM/download?force=true',
-    'https://unsplash.com/photos/mBQIfKlvowM/download?force=true',
-    'https://unsplash.com/photos/A-11N8ItHZo/download?force=true',
-    'https://unsplash.com/photos/kOqBCFsGTs8/download?force=true',
-    'https://unsplash.com/photos/8DMuvdp-vso/download?force=true'
-]
-image_iterator = paginator("Select a sunset page", sunset_imgs)
-indices_on_page, images_on_page = map(list, zip(*image_iterator))
-st.image(images_on_page, width=100, caption=indices_on_page)
 
 
 
