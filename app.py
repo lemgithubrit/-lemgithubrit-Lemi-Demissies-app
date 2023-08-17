67d747bb8099decf09e0e8a99cad8d06b70418ab
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-17 00:55:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -173,7 +173,10 @@ st.sidebar.write('The current color is', color)
 
 st.write("<h1 style='text-align: center; color:white;'> Streamlit is the Fastest Way to Build and Share Data Apps🎈</h1>", unsafe_allow_html=True)
 
-
+ st.vedio(
+            "https://www.youtube.com/watch?v=dbYUeEpRxOc",
+            width=250, 
+        )
 
 
 
