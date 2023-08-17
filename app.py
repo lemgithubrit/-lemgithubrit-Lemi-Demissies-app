c6d074ac7966173258663fc54d4485d348339032
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-16 23:36:41 -0700

    Update app.py

--- app.py
+++ app.py
@@ -165,6 +165,12 @@ st.write("")
 st.write("")
 st.write("")
 st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
 import streamlit as st
 
 col1, col2, col3 = st.columns(3)
@@ -199,7 +205,7 @@ with col2:
 with col3:
     st.write("Phone:-+251921502282")
     
-st.write("<h1 style='text-align: center; color:white;'> Streamlit is the Fastest Way to Build and Share Data Apps🎈</h1>", unsafe_allow_html=True)
+st.write("<h1 style='text-align: left; color:white;'> Streamlit is the Fastest Way to Build and Share Data Apps🎈</h1>", unsafe_allow_html=True)
 
 
 
