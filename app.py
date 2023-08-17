dfd5836c955333fc84fcbfd7b466b8da43a7f367
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-17 08:24:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -31,11 +31,9 @@ option = st.selectbox(
 import streamlit as st
 
 options = st.multiselect(
-    'What are your favorite colors',
     ['Green', 'Yellow', 'Red', 'Blue'],
     ['Yellow', 'Red'])
 
-st.write('You selected:', options)
 
 #ADD TABLE
 import pandas as pd 
