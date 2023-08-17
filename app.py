6a69b826571e951405e352edec8fdc6a95f006a7
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-17 00:25:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ with col2:
   
    st.image(
             "https://previews.123rf.com/images/ingara/ingara1809/ingara180901228/110171575-big-data-vector-visualization-3d-futuristic-cosmic-design-technology-background-visual.jpg",
-            width=270, 
+            width=280, 
         )
 
 with col3:
