3f6f194c6284013f0ac9a38c766f6d9ea1fd7237
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-17 22:30:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ df = pd.DataFrame({'Local Name       ': ['Bishoftu', 'Bora', 'Boset', 'Kora', 'B
 df
 
 
-st.dataframe(df.style.highlight_all(axis=0))
+st.dataframe(df.style.highlight_max(axis=0))
 
 
 
