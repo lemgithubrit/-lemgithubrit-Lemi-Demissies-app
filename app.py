a789e3c203c0630a01f6c01ee9f0f2242d3f2448
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-17 22:57:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -174,11 +174,11 @@ with col3:
 import streamlit as st
 col1, col2, col3 = st.columns(3)
 with col1:
-  st.header("Facebook:-Lem Messa Demissie")
+  st.subheader("Facebook:-Lem Messa Demissie")
 with col2:
-    st.header("Telegram:-+251921502282")
+    st.subheader("Telegram:-+251921502282")
 with col3:
-    st.header("Phone:-+251921502282")
+    st.subheader("Phone:-+251921502282")
 #sidebar   
 date = st.sidebar.date_input("Date")
 text=st.sidebar.text_area("Write Comment Here")
