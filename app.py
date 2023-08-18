16a93744093e99a54aebf33e6a74451e95767549
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-17 22:27:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ df
 df = pd.DataFrame(
    np.random.randn(10, 20),
    columns=('col %d' % i for i in green(20)))
-st.dataframe(df.style.highlight_all(axis=0))
+st.dataframe(df.style.highlight_max(axis=0))
 
 
 
