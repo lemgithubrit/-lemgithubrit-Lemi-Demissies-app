1deba314b35b936ada64e3e7818c056696d3f2cf
lemgithubrit <108199053+lemgithubrit@users.noreply.github.com>
2023-08-17 23:02:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -208,5 +208,23 @@ st.write("<h1 style='text-align: center; color:white;'> Streamlit is the Fastest
 
 
 
+data = {'level_0': [1, 2, 3],
+        'level_1': ['A', 'B', 'C'],
+        '01/23/2023': ['20%', '30%', '40%'],
+        '01/24/2023': ['10%', '20%', '30%'],
+        '01/25/2023': ['15%', '25%', '35%'],
+        '01/26/2023': ['5%', '10%', '15%'],
+        '01/27/2023': ['1%', '2%', '3%']}
 
+df = pd.DataFrame(data)
+df = df.set_index(['level_0', 'level_1'])
+
+def make_bar_style(x):
+    if '%' in str(x):
+        x = float(x.strip('%'))
+        return f"background: linear-gradient(90deg,#5fba7d {x}%, transparent {x}%); width: 10em"  
+    return ''
+
+
+st.dataframe(df.style.applymap(make_bar_style))
 
